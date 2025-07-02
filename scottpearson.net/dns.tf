2728f842bdfe0b42252e48edf77319c739927101
scott-pearson <scott@cloudflare.com>
2025-07-02 17:44:37 +0100

    Changed security rules so I can test with cURL

--- scottpearson.net/dns.tf
+++ scottpearson.net/dns.tf
@@ -2,10 +2,8 @@ resource "cloudflare_dns_record" "dns_0" {
   zone_id = var.cloudflare_zone_id
   name = "scottpearson.net"
   type = "CNAME"
-  comment = "apex DNS record"
   content = "lb.scottpearson.net"
   proxied = true 
-  tags = ["owner:dns-team"]
   ttl = 1
 }
 
@@ -13,9 +11,7 @@ resource "cloudflare_dns_record" "dns_1" {
   zone_id = var.cloudflare_zone_id
   name = "www.scottpearson.net"
   type = "CNAME"
-  comment = "WWW DNS record"
   content = "lb.scottpearson.net"
   proxied = true 
-  tags = ["owner:dns-team"]
   ttl = 1
 }
