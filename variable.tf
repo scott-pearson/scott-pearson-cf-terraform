b5fcb7a916f78ee41608ee7f870c50459d466575
scott-pearson <scott@cloudflare.com>
2025-08-08 15:42:25 +0100

    Updated provider

--- variable.tf
+++ variable.tf
@@ -1,9 +1,3 @@
-#variable "cloudflare_api_token" {
-#  description = "Cloudflare API token with necessary permissions."
-#  type        = string
-#  sensitive   = true
-#}
-
 variable "cloudflare_account_id" {
   description = "Cloudflare account ID"
   type        = string
