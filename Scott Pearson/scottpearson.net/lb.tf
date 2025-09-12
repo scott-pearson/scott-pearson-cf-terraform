c51be9bb195ad6f7b074796a25f043a29f4e94a2
scott-pearson <scott@cloudflare.com>
2025-09-12 08:59:37 +0100

    Added lb geo steering

--- Scott Pearson/scottpearson.net/lb.tf
+++ Scott Pearson/scottpearson.net/lb.tf
@@ -35,7 +35,7 @@ resource "cloudflare_load_balancer_pool" "prod_lb_pool" {
   name = "production"
   description = "production"
   enabled = true
-  monitor = "f200caeafaa828d6a8ea2ed2df2d5440"
+  monitor = cloudflare_load_balancer_monitor.prod_lb_monitor.id
   minimum_origins = 1
   notification_email = "scott@cloudflare.com"
 
@@ -56,24 +56,24 @@ resource "cloudflare_load_balancer_pool" "prod_lb_pool" {
 }
 
 ## bug in https://github.com/cloudflare/terraform-provider-cloudflare/issues/5676
-#resource "cloudflare_load_balancer_monitor" "prod_lb_monitor" {
-#  account_id = var.cloudflare_account_id
-#  description = "production_healthcheck"
-#  type = "https"
-#  port = 443
-#  method = "GET"
-#  path = "/healthcheck.txt"
-#  allow_insecure = false
-#  expected_codes = "200"
-#  follow_redirects = false
-#  consecutive_down = 0
-#  consecutive_up = 0
-#
-#  header = {
-#    Host = ["scottpearson.net"]
-#  }
-#
-#  interval = 60
-#  retries = 2
-#  timeout = 5
-#}
+resource "cloudflare_load_balancer_monitor" "prod_lb_monitor" {
+  account_id = var.cloudflare_account_id
+  description = "production_healthcheck"
+  type = "https"
+  port = 443
+  method = "GET"
+  path = "/healthcheck.txt"
+  allow_insecure = false
+  expected_codes = "200"
+  follow_redirects = false
+  consecutive_down = 0
+  consecutive_up = 0
+  header = {
+    Host = ["scottpearson.net"]
+  }
+  interval = 60
+  retries = 2
+  timeout = 5
+  expected_body = "OK"
+  probe_zone = "scottpearson.net"
+}
